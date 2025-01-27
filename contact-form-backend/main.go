b56a75fdaa70cb6890e656384a867108ab25cbab
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2025-01-27 19:58:49 +0100

    Update main.go

--- contact-form-backend/main.go
+++ contact-form-backend/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type contact struct {
-	id       int
-	fullName string
-	email    string
-	phone    int
-	message  string
+	ID       string `json:"id"`
+	FullName string `json:"fullname"`
+	Email    string `json:"email"`
+	Phone    int    `json:"phone"`
+	Message  string `json:"message"`
 }
 
-var contacts []contact
+var contacts = []contact{
+	{ID: "1", FullName: "AhmedZin",
+		Email:   "ahmed@gmail.com",
+		Phone:   2,
+		Message: "Hi Ahmed"},
+}
+
+func getMessages(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, contacts)
+}
 
 func postMessages(context *gin.Context) {
 
@@ -30,13 +40,31 @@ func postMessages(context *gin.Context) {
 
 }
 
-func getMessages(context *gin.Context) {
-	
+func getContactbyID(id string) (*contact, error) {
+	for i, j := range contacts {
+		if j.ID == id {
+			return &contacts[i], nil
+		}
+	}
+
+	return nil, errors.New("Contact not found")
+}
+
+func getMessage(context *gin.Context) {
+	id := context.Param("id")
+	contact, error := getContactbyID(id)
+
+	if error != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Contact not found"})
+	}
+
+	context.IndentedJSON(http.StatusOK, contact)
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/contactme", postMessages)
 	router.GET("/contactme", getMessages)
+	router.GET("/contactme/:id", getMessage)
 	router.Run() // Default listens on :8080
 }
