c3fa811f74ea6b89af7eb7ebbb89e49e192e4e4e
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2025-01-29 18:26:34 +0100

    update

--- go_backend/main.go
+++ go_backend/main.go
@@ -1,12 +1,33 @@
 package main
 
+//CompileDaemon -command="./go_backend"
+
 import (
+	"context"
+	"log"
+	"os"
+
 	"github.com/ELMEHDAOUIAhmed/MyPortfolio_Win98_Themed/go_backend/initializers"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Example query to test connection
+	var version string
+	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+
+	log.Println("Connected to:", version)
 }
 
 func main() {
