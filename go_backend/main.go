db2dfb4ede4435f8a2a3412907bddc4235394667
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2025-01-30 18:21:23 +0100

    update

--- go_backend/main.go
+++ go_backend/main.go
@@ -3,6 +3,9 @@ package main
 //CompileDaemon -command="./go_backend"
 
 import (
+	"log"
+	"os"
+
 	"github.com/ELMEHDAOUIAhmed/MyPortfolio_Win98_Themed/go_backend/controllers"
 	"github.com/ELMEHDAOUIAhmed/MyPortfolio_Win98_Themed/go_backend/initializers"
 	"github.com/gin-gonic/gin"
@@ -19,5 +22,12 @@ func main() {
 	r.POST("/contactme", controllers.MessageCreate)
 	r.GET("/contactme", controllers.MessagesRetreive)
 	r.GET("/contactme/:id", controllers.MessagesRetreivebyID)
-	r.Run("8080")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := r.Run(":" + port); err != nil {
+		log.Panicf("ERROR :%s", err)
+	}
 }
